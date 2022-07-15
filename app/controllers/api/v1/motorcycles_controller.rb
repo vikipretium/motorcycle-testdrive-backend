07e2884470ccbb4907c092a37261be670a98c6dc
vikipretium <vighneshpasupathy@gmail.com>
2022-07-15 19:37:26 +0530

    refactor class motorcycles controller

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,16 +1,10 @@
-module Api 
-module V1
-  class MotorcyclesController < ApplicationController
+  class Api::V1::MotorcyclesController < ApplicationController
     def index
       @motorcycles = Motorcycle.all
       render json: {status: "SUCCESS", message: "Loaded All Motorcycles", data: @motorcycles}, status: :ok
     end
 
     def show
-      
+
     end
   end
-  
-end
-end
-

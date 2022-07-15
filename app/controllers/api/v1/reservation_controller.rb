e10fa871fe9cbcb55a1e21d40bc9603b21bb7df7
vikipretium <vighneshpasupathy@gmail.com>
2022-07-15 19:12:15 +0530

    generate reservation controller

---
+++ app/controllers/api/v1/reservation_controller.rb
@@ -0,0 +1,16 @@
+class ReservationController < ApplicationController
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
---
+++ spec/requests/reservation_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Reservations", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/reservation/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/reservation/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/reservation/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/reservation/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/reservation/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end

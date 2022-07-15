95f5a24dfc17dca0f94f0746ca3c437b12d5b9b3
daudi13 <davouma13@gmail.com>
2022-07-15 07:44:41 +0300

    add foreign_key table references to user and motorcycle columns in Reservation table

--- db/migrate/20220715044059_create_reservations.rb
+++ db/migrate/20220715044059_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :motorcycle, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: {to_table: 'users'}
+      t.references :motorcycle, null: false, foreign_key: {to_table: 'motorcycles'}
       t.string :city
       t.string :date
 

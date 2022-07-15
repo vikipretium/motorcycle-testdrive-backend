e3b7dc5b4bced7464f85cb06065479b0c84d4106
vikipretium <vighneshpasupathy@gmail.com>
2022-07-15 19:35:50 +0530

    add index action for motorcycles

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,7 +1,16 @@
-class MotorcyclesController < ApplicationController
-  def index
-  end
+module Api 
+module V1
+  class MotorcyclesController < ApplicationController
+    def index
+      @motorcycles = Motorcycle.all
+      render json: {status: "SUCCESS", message: "Loaded All Motorcycles", data: @motorcycles}, status: :ok
+    end
 
-  def show
+    def show
+      
+    end
   end
+  
 end
+end
+

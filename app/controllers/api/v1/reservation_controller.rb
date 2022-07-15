0c27e2b30ebdc76e0b9934db11b8efb2f560182d
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-15 21:27:03 +0545

    Inside index we call all reservation and convert and into json

--- app/controllers/api/v1/reservation_controller.rb
+++ app/controllers/api/v1/reservation_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ReservationController < ApplicationController
   def index; 
+  @reservations = Reservation.all
+  render json: {status: "SUCCESS", message: "Loaded all reservation", data: @reservations }, status: :ok
   end
 
   def destroy; end

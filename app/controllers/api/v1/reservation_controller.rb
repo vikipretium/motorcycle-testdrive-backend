b41557cdd424fda5928193ffd4eb5df6a27807be
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-15 21:51:46 +0545

    Add action create for reservation

--- app/controllers/api/v1/reservation_controller.rb
+++ app/controllers/api/v1/reservation_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ReservationController < ApplicationController
   def index; 
-  @reservations = Reservation.all
-  render json: {status: "SUCCESS", message: "Loaded all reservation", data: @reservations }, status: :ok
+  reservations = Reservation.all
+  render json: {status: "SUCCESS", message: "Loaded all reservation", data: reservations }, status: :ok
   end
 
   def destroy;
@@ -10,11 +10,19 @@ class Api::V1::ReservationController < ApplicationController
   end
 
   def show; 
-  @reservation = Reservation.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'Loaded reservation', data: @reservation }, status: :ok
+  reservation = Reservation.find(params[:id])
+    render json: { status: 'SUCCESS', message: 'Loaded reservation', data: reservation }, status: :ok
   end
 
-  def create; end
+  def create;
+  user = User.create!(user_params)
+  motorcycle = Motorcycle.create!(motorcycle_params)
+  reservation = Reservation.new(reservation_params) 
+  if reservation.save
+    render json: {status: 'SUCCESS', message: 'reservation save', data: reservation }, status: :ok
+  else
+    render json: {status: 'ERROR', message: 'reservation is not saved', data: reservation.errors }, status: :unprocessable_entity
+  end
 
   def update; end
 end

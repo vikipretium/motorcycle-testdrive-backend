27a69bc6f97cc57e255bdc734ff4899fc8a7d29c
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 12:49:56 +0200

    Reload schema to match new user requirement

--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_044059) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "full_name"
     t.string "email"
-    t.integer "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

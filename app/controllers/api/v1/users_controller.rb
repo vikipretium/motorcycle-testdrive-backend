995fb2114c156313fad4c0c50359ce8a50f564e6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 12:46:56 +0200

    Add index method to user controller to get all Users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
 
+  def index 
+    users = User.all
+    if users
+      render json: { status: 'SUCCESS', message: 'Users loaded', data: users }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Users not loaded', data: users.errors }, status: :unprocessable_entity
+    end
+  end
+
   def update
   end
 

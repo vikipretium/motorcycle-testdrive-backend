e9eda9b471c2176d3bed622bb226d27fbd96b2d0
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 12:44:28 +0200

    Fix relationship associations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :motorcycles, through: :reservations, dependent: :destroy
-  has_many :motorcycles
 
   validates :full_name, :email, presence: true
 end

68e7aba93b8ba139741b9613592784e0f1b5cbe4
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 12:46:20 +0200

    Add private methods to set user_params and set_current_user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,25 +1,16 @@
-class UserController < ApplicationController
-  def index; end
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
 
-  def destroy; end
-
-  def show; end
-
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: { message: 'User saved', data: user }, status: :ok
-    else
-      render json: { status: 'ERROR', message: 'user has not been saved try again', data: user.errors },
-             status: :unprocessable_entity
-    end
+  def update
   end
-
-  def update; end
 
   private
 
   def user_params
     params.permit(:first_name, :last_name, :email, :phone_number)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

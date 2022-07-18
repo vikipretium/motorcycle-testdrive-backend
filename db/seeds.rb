1f1826eb7fb2c125310a253c45b726ffbea99836
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-18 14:27:26 +0545

    Add data on seed file that is user and for reservation

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ Motorcycle.create([{name:"KTM 1290 SUPER DUKE R EVO",image:"https://azwecdnepsto
 
         The legend of Fireblade has evolved to grow bigger with time, and in 2020 we are here with two new variants created along with Honda Racing Corporation. The CBR1000RR-R Fireblade and CBR1000RR-R Fireblade SP are worthy of furthering the legacy of the original Fireblade.",price:28900}])
 
-User.create(first_name: "Roshan", last_name: "Bajgain", email: "ros@gmail.com", number: 9842700880)    
-User.create(first_name: "Vignash", last_name: "singh", email: "vignas@gmail.com", number: 9842700880)   
-User.create(first_name: "David", last_name: "ouma", email: "david@gmail.com", number: 9842700880)   
-User.create(first_name: "Alick", last_name: "Nyirenda", email: "alick@gmail.com", number: 9842700880)  
+User.create(first_name: "Roshan", last_name: "Bajgain", email: "ros@gmail.com", phone_number: 9842)    
+User.create(first_name: "Vignash", last_name: "singh", email: "vignas@gmail.com", phone_number: 7880)
+User.create(first_name: "David", last_name: "ouma", email: "david@gmail.com", phone_number: 7880)   
+User.create(first_name: "Alick", last_name: "Nyirenda", email: "alick@gmail.com", phone_number: 9880)  
 
-Reservation.create(user_id: 1, motorcycle_id: 1, city: "london")
-Reservation.create(user_id: 2, motorcycle_id: 2, city: "Kathmandu")
-Reservation.create(user_id: 3, motorcycle_id: 3, city: "Delhi")
-Reservation.create(user_id: 4, motorcycle_id: 4, city: "paris")
+Reservation.create(user_id: 1, motorcycle_id: 1, city: "london",date: 2022-01-10)
+Reservation.create(user_id: 2, motorcycle_id: 2, city: "Kathmandu",date: 2022-02-15)
+Reservation.create(user_id: 3, motorcycle_id: 3, city: "Delhi",date: 2022-03-17)
+Reservation.create(user_id: 4, motorcycle_id: 4, city: "paris", date: 2022-04-20)

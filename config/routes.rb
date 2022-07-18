7aedb3aea8d3cba6733f3dd6f87ab578cc5255f5
daudi13 <davouma13@gmail.com>
2022-07-18 07:56:00 +0300

    refactor users and reservations routes without actions

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   namespace :api do 
   namespace :v1 do
     resources :motorcycles, only: [:index,:show]
-    resources :user, only: [:index,:destroy,:show,:create,:update] do
-      resources :reservations, only: [:index,:destroy,:show,:create,:update]
+    resources :users do
+      resources :reservations
     end
   end
   end

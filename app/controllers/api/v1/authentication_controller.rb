3d7db0ecb290187f0b4a25e53b57975eeeaa5519
daudi13 <davouma13@gmail.com>
2022-07-19 12:57:33 +0300

    add token variable inside json response

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,9 +4,9 @@ rescue_from ActionController::ParameterMissing, with: :parameter_missing
   def create
     p params.require(:password).inspect
     user = User.find_by(email: params.require(:email))
-    AuthenticationTokenService.call(user.id)
-    
-    render json: { token: '123' }, status: :created
+    token = AuthenticationTokenService.call(user.id)
+
+    render json: { token: token }, status: :created
   end
 
   def parameter_missing(e)

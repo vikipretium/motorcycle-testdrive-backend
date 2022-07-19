4f41dc512dd3657e7ed1e8b5a341e375b5910178
daudi13 <davouma13@gmail.com>
2022-07-19 11:29:29 +0300

    add test for invalid email and password

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::AuthenticationController < ApplicationController
   def create
     p params.inspect
+
+    render json: { token: '123' }, status: :created
   end
 end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -5,7 +5,21 @@ describe 'Authentication', type: :request do
     it 'should authenticate the client' do
       post '/api/v1/authenticate', params: { email: "david@gmail.com", password: "password1"}
       expect(response).to have_http_status(:created)
-      expect(response_body).to eq('token')
+      expect(response.body).to include('token')
     end
+
+    it 'should return error if invalid email' do
+      post '/api/v1/authenticate', params: { password: "password1"}
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'should return error if invalid password' do
+      post '/api/v1/authenticate', params: { email: "david@gmail.com" }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+
   end
 end

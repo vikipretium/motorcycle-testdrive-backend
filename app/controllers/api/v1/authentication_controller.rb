3d38aa119dc26b18739278f852a7e8f8bae025a5
daudi13 <davouma13@gmail.com>
2022-07-19 11:34:06 +0300

    add parameter_missing method

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::AuthenticationController < ApplicationController
+rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
   def create
-    p params.inspect
+    p params.require(:email).inspect
+    p params.require(:password).inspect
 
     render json: { token: '123' }, status: :created
+  end
+
+  def parameter_missing(e)
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 end

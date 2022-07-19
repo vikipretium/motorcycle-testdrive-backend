ff361d34d6933b849b33fe0603a1a4ebba676059
daudi13 <davouma13@gmail.com>
2022-07-19 17:41:29 +0300

    rewrite test

--- spec/requests/motorcycles_spec.rb
+++ spec/requests/motorcycles_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Motorcycles', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/motorcycles/index'
-      expect(response).to have_http_status(:success)
-    end
+  let!(:motorcycle) do
+    FactoryBot.create(:motorcycle, name: 'Honda', image: 'image.jpg', specification: 'specification', price: 100) 
+    FactoryBot.create(:motorcycle, name: 'Honda', image: 'image.jpg', specification: 'specification', price: 100) 
+    FactoryBot.create(:motorcycle, name: 'Honda', image: 'image.jpg', specification: 'specification', price: 100) 
   end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/motorcycles/show'
+  
+  describe 'GET /motorcycles' do
+    it 'should return all motorcycles' do
+      get '/api/v1/motorcycles'
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(3)
     end
   end
 end

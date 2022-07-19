8847ac70d52cbd8d287c99414babbac7b0cb9d0c
daudi13 <davouma13@gmail.com>
2022-07-19 13:42:28 +0300

    refactor user params with newly created user variable

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -5,20 +5,20 @@ describe 'Authentication', type: :request do
     let(:user) {User.create(full_name: 'John', email: 'john@gmail.com', password: '123456')}
 
     it 'should authenticate the client' do
-      post '/api/v1/authenticate', params: { email: "david@gmail.com", password: "password1"}
+      post '/api/v1/authenticate', params: { email: user.email, password: user.password }
       expect(response).to have_http_status(:created)
       expect(response.body).to include('token')
     end
 
     it 'should return error if invalid email' do
-      post '/api/v1/authenticate', params: { password: "password1"}
+      post '/api/v1/authenticate', params: { password: user.password }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body).to include('error')
     end
 
     it 'should return error if invalid password' do
-      post '/api/v1/authenticate', params: { email: "david@gmail.com" }
+      post '/api/v1/authenticate', params: { email: user.email }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body).to include('error')

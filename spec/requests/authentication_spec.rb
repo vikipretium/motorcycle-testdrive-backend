eb4e8f4fc1625fb02aae885f5607dcf3bcff37bb
daudi13 <davouma13@gmail.com>
2022-07-19 10:30:20 +0300

    add test outline for authentication

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 describe 'Authentication', type: :request do
+  describe 'POST /authenticate' do
+    it 'should authenticate the client' do
+      post '/api/v1/authenticate'
+    end
+  end
 end

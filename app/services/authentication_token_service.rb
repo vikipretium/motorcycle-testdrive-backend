6fc047e41a67bdb1f87e3a5a20662d594c6b4516
daudi13 <davouma13@gmail.com>
2022-07-19 12:23:07 +0300

    refactor test form refactored class

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,9 +1,10 @@
 class AuthenticationTokenService
   HMAC_SECRET = 'my$ecretK3y'
+  ALGORITHM_TYPE = 'HS256'
   
   
   def self.call
     payload = {"test" => "test"}
-    JWT.encode payload, HMAC_SECRET, 'HS256'
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe AuthenticationTokenService do
+  let(:token) { described_class.call }
+  
   describe '.call' do
     it 'returns an authentication token' do
-      token = described_class.call
-      decoded_token = JWT.decode token, described_class::HMAC_SECRET, true, { algorithm: 'HS256' }
+      decoded_token = JWT.decode(
+        token,
+        described_class::HMAC_SECRET, 
+        true, 
+        { algorithm: described_class::ALGORITHM_TYPE }
+        )
 
       expect(decoded_token).to eq(
         [

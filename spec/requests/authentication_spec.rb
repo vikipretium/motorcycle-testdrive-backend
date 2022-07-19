c6c6d884c9ea398c616ceeef857b5f81a6064de4
daudi13 <davouma13@gmail.com>
2022-07-19 13:41:46 +0300

    create globla user for test

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
+    let(:user) {User.create(full_name: 'John', email: 'john@gmail.com', password: '123456')}
+
     it 'should authenticate the client' do
       post '/api/v1/authenticate', params: { email: "david@gmail.com", password: "password1"}
       expect(response).to have_http_status(:created)

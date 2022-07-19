79241f98c540150e3fe1ee9f21fc5b3f118fdfad
daudi13 <davouma13@gmail.com>
2022-07-19 12:11:54 +0300

    add conclusive test for .call

--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 describe AuthenticationTokenService do
   describe '.call' do
     it 'returns an authentication token' do
-      expect(described_class.call).to eq('123')
+      token = described_class.call
+      hmac_secret = 'my$ecretK3y'
+      decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+
+      expect(decoded_token).to eq(
+        [
+          {"test"=>"test"},
+          {"alg"=>"HS256"} 
+        ]
+      )
     end
   end
 end

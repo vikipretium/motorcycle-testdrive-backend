e86d145847e012102f4412023acf27b07f4b21ec
daudi13 <davouma13@gmail.com>
2022-07-23 06:17:39 +0300

    add respose body to destroy action in userController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    head :no_content
+    render json: { status: 'SUCCESS', message: 'User deleted', data: @user }, status: :ok
   end
 
   def show
@@ -38,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :phone_number)
+    params.permit(:full_name, :email, :password)
   end
 
   def set_user

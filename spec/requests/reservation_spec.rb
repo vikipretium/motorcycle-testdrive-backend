df85d28a2a13803790a0ecb4f41fb6d36e96a3b7
daudi13 <davouma13@gmail.com>
2022-07-25 18:14:57 +0300

    add test for returning a user's reservation

--- spec/requests/reservation_spec.rb
+++ spec/requests/reservation_spec.rb
@@ -1,38 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Reservations', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/reservation/index'
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe 'Reservations', type: :request do 
+  before(:all) do
+    @motorcycle = Motorcycle.create!(name: "bike 1", specification: 'specification 1', price: 100, image: 'image 1')
+    @user = User.create!(full_name: "user 1", email: "email 1", password: "password 1")
+    @reservation = Reservation.create!(user_id: @user.id, motorcycle_id: @motorcycle.id, date: "2020-01-01", city: "city 1")
+    @reservation = Reservation.create!(user_id: @user.id, motorcycle_id: @motorcycle.id, date: "2020-01-01", city: "city 1")
   end
 
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/reservation/destroy'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'ap1/v1/users/:id/reservation' do
+    it 'returns a user reservation' do
+      get "/api/v1/users/#{@user.id}/reservations"
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/reservation/show'
       expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/reservation/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/reservation/update'
-      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["data"].size).to eq(2)
     end
   end
 end

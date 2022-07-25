dad4ef99d3f8934571370a389cdbc5ca7ae6a483
daudi13 <davouma13@gmail.com>
2022-07-25 21:59:18 +0300

    changes path

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -5,27 +5,27 @@ describe 'Authentication', type: :request do
     let(:user) { User.create(full_name: 'John', email: 'john@gmail.com', password: '123456') }
 
     it 'should authenticate the client' do
-      post '/api/v1/authenticate', params: { email: user.email, password: user.password }
+      post '/api/v1/register', params: { email: user.email, password: user.password }
       expect(response).to have_http_status(:created)
       expect(response.body).to include('token')
     end
 
     it 'should return error if invalid email' do
-      post '/api/v1/authenticate', params: { password: user.password }
+      post '/api/v1/login', params: { password: user.password }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body).to include('error')
     end
 
     it 'should return error if invalid password' do
-      post '/api/v1/authenticate', params: { email: user.email }
+      post '/api/v1/login', params: { email: user.email }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body).to include('error')
     end
 
     it 'should return error if invalid email and password' do
-      post '/api/v1/authenticate', params: { email: user.email, password: 'invalid' }
+      post '/api/v1/login', params: { email: user.email, password: 'invalid' }
 
       expect(response).to have_http_status(:unauthorized)
     end

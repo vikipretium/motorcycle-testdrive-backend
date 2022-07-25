cba6d502c5a96402fad788bbaf753a46b6d86f18
daudi13 <davouma13@gmail.com>
2022-07-25 20:50:59 +0300

    add test for User SHOW

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   before(:all) do 
-    @user = User.create!([{ full_name: "user 1", email: "email 1", password: "password 1" }, { full_name: "user 2", email: "email 2", password: "password 2" }])
+    @user = User.create!( full_name: "user 1", email: "email 1", password: "password")
   end
 
   describe 'ap1/v1/users' do
@@ -10,7 +10,17 @@ RSpec.describe 'Users', type: :request do
       get '/api/v1/users'
 
       expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body)["data"].size).to eq(2)
+      expect(JSON.parse(response.body)["data"].size).to eq(1)
     end
+  end
+
+  describe 'ap1/v1/users/:id' do
+    it ' returns a specif user' do
+      get "/api/v1/users/#{@user.id}"
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["data"]["full_name"]).to eq("user 1")
+    end
+
   end
 end

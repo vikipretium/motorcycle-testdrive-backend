370b42e8e71d3a7fa4e9caa18f149ce656031940
daudi13 <davouma13@gmail.com>
2022-07-25 20:46:02 +0300

    add test for user Index

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,38 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/user/index'
-      expect(response).to have_http_status(:success)
-    end
+  before(:all) do 
+    @user = User.create!([{ full_name: "user 1", email: "email 1", password: "password 1" }, { full_name: "user 2", email: "email 2", password: "password 2" }])
   end
 
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/user/destroy'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'ap1/v1/users' do
+    it 'returns all users' do
+      get '/api/v1/users'
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/user/show'
       expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/user/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/user/update'
-      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["data"].size).to eq(2)
     end
   end
 end

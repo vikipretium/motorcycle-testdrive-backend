76e6559aa4b1d32b1b59e9bcb27f365a96b3cc58
daudi13 <davouma13@gmail.com>
2022-07-27 11:23:58 +0300

    generate respective rspec files

---
+++ spec/requests/api/v1/authentication_spec.rb
@@ -0,0 +1,38 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/authentication', type: :request do
+
+  path '/api/v1/login' do
+
+    post('create authentication') do
+      response(200, 'successful') do
+        
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/authenticate' do
+
+    get('token_validation authentication') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
